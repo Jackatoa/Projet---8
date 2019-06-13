694d4c64051cbc112c1c6200dc7735f4f4b35587
Jackatoa <xavier.endres@gmail.com>
2019-06-13 15:32:00 +0200

    rajout des modeles de db

--- main_home/models.py
+++ main_home/models.py
@@ -3,10 +3,25 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Aliment(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=100)
+    url = models.TextField()
+    url_img = models.TextField()
+    url_nutri = models.TextField()
+    category = models.TextField()
+    stores = models.TextField()
+    nutriscore = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
+
+class AlimentSaved(models.Model):
+    urloriginal = models.ForeignKey('Aliment', on_delete=models.PROTECT,
+                                    related_name='originals')
+    urlsubstitute = models.ForeignKey('Aliment', on_delete=models.PROTECT,
+                                    related_name='substituted')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.author
