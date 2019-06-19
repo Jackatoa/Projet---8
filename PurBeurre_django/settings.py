9428ac6b669578a67904a60d92d8f5727393397a
Jackatoa <xavier.endres@gmail.com>
2019-06-19 13:51:00 +0200

    rajout de django_heroku

--- PurBeurre_django/settings.py
+++ PurBeurre_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,3 +135,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'main_home-home'
 LOGIN_URL = 'login'
 
+django_heroku.settings(locals())
