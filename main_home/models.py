d5ba1f6b340e5112fd7bb66ce5c85648fc5fd17a
Jackatoa <xavier.endres@gmail.com>
2019-06-24 17:17:13 +0200

    fix model

--- main_home/models.py
+++ main_home/models.py
@@ -10,7 +10,7 @@ class Aliment(models.Model):
     category = models.TextField()
     stores = models.TextField()
     nutriscore = models.CharField(max_length=1)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
 
     def __str__(self):
         return self.name
