bdc5fdb64320af97d2bb46f7797aa4b860466eea
Jackatoa <xavier.endres@gmail.com>
2019-06-24 17:20:57 +0200

    fix

--- main_home/migrations/0003_aliment_author.py
+++ main_home/migrations/0003_aliment_author.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='aliment',
             name='author',
-            field=models.ForeignKey(default='admin', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default='1', on_delete=django.db.models.deletion.CASCADE,
+                                    to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
     ]
